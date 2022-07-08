32c8523e75d7b8f3c6b39906db7dd7d8e4834729
Jonathan Glock <glockjt@gmail.com>
2022-07-08 09:25:12 -0600

    Initial test.

---
+++ projects-with-changes.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+BRANCH_TO_COMPARE="$*"
+
+GIT_DIFF=$(git --no-pager diff --name-only main...$BRANCH_TO_COMPARE)
+# GIT_DIFF=$(git --no-pager diff --name-only develop...develop)
+PROJECTS_CHANGED=""
+# echo $GIT_DIFF
+if [[ ! -z "$GIT_DIFF" ]]
+then
+    if [[ $GIT_DIFF == *"next"* ]]
+    then
+        PROJECTS_CHANGED="next"
+    fi
+
+    if [[ $GIT_DIFF == *"mobile"* ]]
+    then
+        [[ ! -z "$PROJECTS_CHANGED" ]] && PROJECTS_CHANGED="${PROJECTS_CHANGED}/mobile" || PROJECTS_CHANGED="mobile"
+    fi
+
+    if [[ $GIT_DIFF == *"node"* ]]
+    then
+        [[ ! -z "$PROJECTS_CHANGED" ]] && PROJECTS_CHANGED="${PROJECTS_CHANGED}/node" || PROJECTS_CHANGED="node"
+    fi
+    echo $PROJECTS_CHANGED
+fi
