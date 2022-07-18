988857d23fdf17776a3a7025b7257adf82c20093
Jonathan Glock <glockjt@gmail.com>
2022-07-18 14:46:37 -0600

    Fetch main.

--- projects-with-changes.sh
+++ projects-with-changes.sh
@@ -2,28 +2,29 @@
 
 BRANCH_TO_COMPARE="$*"
 
-git remote -v
-# GIT_DIFF=$(git --no-pager diff --name-only main...$BRANCH_TO_COMPARE)
-# # GIT_DIFF=$(git --no-pager diff --name-only develop...develop)
-# PROJECTS_CHANGED=""
-# # echo $GIT_DIFF
-# if [[ ! -z "$GIT_DIFF" ]]
-# then
-#     if [[ $GIT_DIFF == *"next"* ]]
-#     then
-#         PROJECTS_CHANGED="next"
-#     fi
+git remote set-branches --add origin main
+git fetch
+GIT_DIFF=$(git --no-pager diff --name-only origin/main...$BRANCH_TO_COMPARE)
+# GIT_DIFF=$(git --no-pager diff --name-only develop...develop)
+PROJECTS_CHANGED=""
+# echo $GIT_DIFF
+if [[ ! -z "$GIT_DIFF" ]]
+then
+    if [[ $GIT_DIFF == *"next"* ]]
+    then
+        PROJECTS_CHANGED="next"
+    fi
 
-#     if [[ $GIT_DIFF == *"mobile"* ]]
-#     then
-#         [[ ! -z "$PROJECTS_CHANGED" ]] && PROJECTS_CHANGED="${PROJECTS_CHANGED}/mobile" || PROJECTS_CHANGED="mobile"
-#     fi
+    if [[ $GIT_DIFF == *"mobile"* ]]
+    then
+        [[ ! -z "$PROJECTS_CHANGED" ]] && PROJECTS_CHANGED="${PROJECTS_CHANGED}/mobile" || PROJECTS_CHANGED="mobile"
+    fi
 
-#     if [[ $GIT_DIFF == *"node"* ]]
-#     then
-#         [[ ! -z "$PROJECTS_CHANGED" ]] && PROJECTS_CHANGED="${PROJECTS_CHANGED}/node" || PROJECTS_CHANGED="node"
-#     fi
-#     echo $PROJECTS_CHANGED
-# else
-#   echo "test"
-# fi
+    if [[ $GIT_DIFF == *"node"* ]]
+    then
+        [[ ! -z "$PROJECTS_CHANGED" ]] && PROJECTS_CHANGED="${PROJECTS_CHANGED}/node" || PROJECTS_CHANGED="node"
+    fi
+    echo $PROJECTS_CHANGED
+else
+  echo "test"
+fi
