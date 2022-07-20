29bc68ac831b7787d89270d4b12b11e38d2f6703
Jonathan Glock <glockjt@gmail.com>
2022-07-20 10:50:18 -0600

    Added for loop.

--- travis-scripts/projects-with-changes.sh
+++ travis-scripts/projects-with-changes.sh
@@ -5,37 +5,54 @@ BRANCH_TO_COMPARE="$*"
 git remote set-branches --add origin main
 git fetch
 
-GIT_DIFF=$(git --no-pager diff --name-only origin/main...$BRANCH_TO_COMPARE)
-# GIT_DIFF=$(git --no-pager diff --name-only develop...develop)
-PROJECTS_CHANGED="parent"
-
-echo GIT_DIFF
-echo -e $GIT_DIFF
+GIT_DIFF=($(git --no-pager diff --name-only origin/main...$BRANCH_TO_COMPARE))
 
 NEXT_DIRTY=""
 MOBILE_DIRTY=""
 NODE_DIRTY=""
 
-if [[ ! -z "$GIT_DIFF" ]]
-then
-    if [[ $GIT_DIFF == "next"* ]]
+for file in "${GIT_DIFF[@]}"
+do
+    if [[ $file == "next"* ]]
     then
-        PROJECTS_CHANGED="next"
         NEXT_DIRTY="yes"
     fi
 
-    if [[ $GIT_DIFF == "mobile"* ]]
+    if [[ $file == "mobile"* ]]
     then
         MOBILE_DIRTY="yes"
-        [[ ! -z "$PROJECTS_CHANGED" ]] && PROJECTS_CHANGED="${PROJECTS_CHANGED}/mobile" || PROJECTS_CHANGED="mobile"
     fi
 
-    if [[ $GIT_DIFF == "node"* ]]
+    if [[ $file == "node"* ]]
     then
         NODE_DIRTY="yes"
-        [[ ! -z "$PROJECTS_CHANGED" ]] && PROJECTS_CHANGED="${PROJECTS_CHANGED}/node" || PROJECTS_CHANGED="node"
     fi
-    # echo $PROJECTS_CHANGED
-else
-  echo "none"
-fi
+done
+
+# echo NEXT_DIRTY $NEXT_DIRTY
+# echo NODE_DIRTY $NODE_DIRTY
+# echo MOBILE_DIRTY $MOBILE_DIRTY
+
+# if [[ ! -z "$GIT_DIFF" ]]
+# then
+#     if [[ $GIT_DIFF == *"next"* ]]
+#     then
+#         PROJECTS_CHANGED="next"
+#         NEXT_DIRTY="yes"
+#     fi
+
+#     if [[ $GIT_DIFF == "mobile"* ]]
+#     then
+#         MOBILE_DIRTY="yes"
+#         [[ ! -z "$PROJECTS_CHANGED" ]] && PROJECTS_CHANGED="${PROJECTS_CHANGED}/mobile" || PROJECTS_CHANGED="mobile"
+#     fi
+
+#     if [[ $GIT_DIFF == *"node"* ]]
+#     then
+#         NODE_DIRTY="yes"
+#         [[ ! -z "$PROJECTS_CHANGED" ]] && PROJECTS_CHANGED="${PROJECTS_CHANGED}/node" || PROJECTS_CHANGED="node"
+#     fi
+#     # echo $PROJECTS_CHANGED
+# else
+#   echo "none"
+# fi
